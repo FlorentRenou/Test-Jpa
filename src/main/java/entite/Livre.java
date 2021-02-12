efd8296862a21622f854740bed2c1b7994900d83
RENOU Florent <florent.renou01@gmail.fr>
2021-02-12 15:32:49 +0100

    Initial commit

---
+++ src/main/java/entite/Livre.java
@@ -0,0 +1,56 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+public class Livre implements Serializable {
+
+    @Id
+    private int id;
+    private String titre;
+    private String auteur;
+
+    public Livre() {
+    }
+
+    public Livre(int id, String titre, String auteur) {
+        this.id = id;
+        this.titre = titre;
+        this.auteur = auteur;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getAuteur() {
+        return auteur;
+    }
+
+    public void setAuteur(String auteur) {
+        this.auteur = auteur;
+    }
+
+    @Override
+    public String toString() {
+        return "Livre{" +
+                "id=" + id +
+                ", titre=" + titre +
+                ", auteur=" + auteur +
+                '}';
+    }
+}
